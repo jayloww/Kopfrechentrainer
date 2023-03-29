a737f0f6a61acbd99d5c0a2fd4ef5de9e3d12610
jayloww <jakob.lauterwasser@web.de>
2023-03-29 16:45:24 +0200

    chore: formatting

--- src/main.cpp
+++ src/main.cpp
@@ -4,40 +4,45 @@ int main()
 {
     std::cout << "***************************************" << std::endl;
     std::cout << "* Willkommen beim Kopfrechentraining. *" << std::endl;
-    std::cout << "***************************************\n" << std::endl;
+    std::cout << "***************************************\n"
+              << std::endl;
     int number = 0;
-    while(true){
-    std::cout << "Hauptmenü" << std::endl;
-    std::cout << "=========\n" << std::endl;
+    while (true)
+    {
+        std::cout << "Hauptmenü" << std::endl;
+        std::cout << "=========\n"
+                  << std::endl;
 
-    std::cout << "1) Einmaleins" << std::endl;
-    std::cout << "2) Plus/Minus" << std::endl;
-    std::cout << "3) Quadratzahlen" << std::endl;
-    std::cout << "4) Spiel verlassen\n" << std::endl;
+        std::cout << "1) Einmaleins" << std::endl;
+        std::cout << "2) Plus/Minus" << std::endl;
+        std::cout << "3) Quadratzahlen" << std::endl;
+        std::cout << "4) Spiel verlassen\n"
+                  << std::endl;
 
-    std::cout << "Was wollen sie üben ?" << std::endl;
-    std::cin >> number;
-    switch(number){
+        std::cout << "Was wollen sie üben ?" << std::endl;
+        std::cin >> number;
+        switch (number)
+        {
         case 1:
-        std::cout << "Einmaleins" << std::endl;
-        break;
-        
+            std::cout << "Einmaleins" << std::endl;
+            break;
+
         case 2:
-        std::cout << "Plus/Minus" << std::endl;
-        break;
+            std::cout << "Plus/Minus" << std::endl;
+            break;
 
         case 3:
-        std::cout << "Quadratzahlen" << std::endl;
-        break;
+            std::cout << "Quadratzahlen" << std::endl;
+            break;
 
         case 4:
-        std::cout << "Sie haben das spiel beendet" << std::endl;
-        return 0;
-        break;
+            std::cout << "Sie haben das spiel beendet" << std::endl;
+            return 0;
+            break;
 
         default:
-        std::cout << "Bitte geben Sie eine Zahl zwischen 1 und 4 ein: ";
-        break;
-    }
+            std::cout << "Bitte geben Sie eine Zahl zwischen 1 und 4 ein!\n ";
+            break;
+        }
     }
 }
