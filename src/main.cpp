0eb61bd98554f1bc513b3d5367ee5346685a07c3
jayloww <jakob.lauterwasser@web.de>
2023-03-29 16:08:43 +0200

    Initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,39 @@ int main()
     std::cout << "***************************************" << std::endl;
     std::cout << "* Willkommen beim Kopfrechentraining. *" << std::endl;
     std::cout << "***************************************\n" << std::endl;
-
     int number = 0;
-    std::cout << "Bitte geben Sie eine Zahl zwischen 1 und 3 ein: ";
-    std::cin >> number;
-    std::cout << "Sie haben " << number << " eingegeben." << std::endl;
+    while(true){
+    std::cout << "Hauptmenü" << std::endl;
+    std::cout << "=========\n" << std::endl;
 
-    return 0;
+    std::cout << "1) Einmaleins" << std::endl;
+    std::cout << "2) Plus/Minus" << std::endl;
+    std::cout << "3) Quadratzahlen" << std::endl;
+    std::cout << "4) Spiel verlassen\n" << std::endl;
+
+    std::cout << "Was wollen sie üben ?" << std::endl;
+    std::cin >> number;
+    switch(number){
+        case 1:
+        std::cout << "Einmaleins" << std::endl;
+        break;
+        
+        case 2:
+        std::cout << "Plus/Minus" << std::endl;
+        break;
+
+        case 3:
+        std::cout << "Quadratzahlen" << std::endl;
+        break;
+
+        case 4:
+        std::cout << "Sie haben das spiel beendet" << std::endl;
+        return 0;
+        break;
+
+        default:
+        std::cout << "Bitte geben Sie eine Zahl zwischen 1 und 4 ein: ";
+        break;
+    }
+    }
 }
